fe9bef7cdd97084cb502a68bb8eda91f025f724f
Liana Grigoryan <lgrigoryan25@gmail.com>
2020-12-09 23:06:31 +0400

    Project add

---
+++ DiscriminantCounter/Discriminant.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace DiscriminantCounter
+{
+    class Discriminant
+    {
+        double a, b, c;
+        public double D { get; set; }
+        public double X1 { get; set; }
+        public double X2 { get; set; }
+
+        public Discriminant(double a, double b, double c)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+
+        public void countDiscriminat()
+        {
+            D = Math.Pow(b, 2) - 4 * a * c;
+            if (D == 0)
+            {
+                X1 = -b / (2 * a);
+            }
+            else if (D > 0)
+            {
+                X1 = (-b + Math.Sqrt(D)) / (2 * a);
+                X2 = (-b - Math.Sqrt(D)) / (2 * a);
+            }
+            //else
+            //{
+            //    yield return "There is no real solution";
+            //}
+        }
+    }
+}
---
+++ DiscriminantCounter/MainWindow.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DiscriminantCounter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCount_Click(object sender, RoutedEventArgs e)
+        {
+            reset();
+            if (!areTextBoxesEmpty())
+            {
+                double a = double.Parse(tboxA.Text);
+                double b = double.Parse(tboxB.Text);
+                double c = double.Parse(tboxC.Text);
+                if (isNull(a))
+                {
+                    tboxError.Visibility = Visibility.Visible;
+                }
+                else
+                {
+                    Discriminant discriminant = new Discriminant(a, b, c);
+                    discriminant.countDiscriminat();
+                    layoutResult.Visibility = Visibility.Visible;
+                    labelD.Content = labelD.Content?.ToString() + discriminant.D;
+                    labelX1.Content = labelX1.Content?.ToString() + string.Format("{0:F1}", discriminant.X1);
+                    labelX2.Content = labelX2.Content?.ToString() + string.Format("{0:F1}", discriminant.X2);
+                }
+            }
+        }
+
+        private void reset()
+        {
+            labelD.Content = "D   =   ";
+            labelX1.Content = "X1   =   ";
+            labelX2.Content = "X2   =   ";
+            tboxError.Visibility = Visibility.Hidden;
+            layoutResult.Visibility = Visibility.Hidden;
+        }
+
+        private bool areTextBoxesEmpty()
+        {
+            if (String.IsNullOrEmpty(tboxA.Text) ||
+              String.IsNullOrEmpty(tboxB.Text) ||
+              String.IsNullOrEmpty(tboxC.Text))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private bool isNull(double value)
+        {
+            if (value == 0) 
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private void info_click(object sender, RoutedEventArgs e)
+        {
+            Process.Start("https://www.mathsisfun.com/algebra/quadratic-equation.html");
+        }
+    }
+}
